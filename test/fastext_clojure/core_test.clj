16f29aafbfbf9be345ffb86ccf489b204c810ded
Arnaud Schenk <arnaud@joinef.com>
2017-01-30 08:58:31 +0000

    Working commit

---
+++ test/fastext_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns fastext-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [fastext-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
