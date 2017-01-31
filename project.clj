57c8ac85bcd8369da60ffffd67c591fe47f27d6e
Arnaud Schenk <arnaud@joinef.com>
2017-01-31 16:21:17 +0000

    Finish the parsing, on to the model

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :jvm-opts ["-Xmx6g"]
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.mikera/vectorz-clj "0.45.0"]]
+                 [uncomplicate/neanderthal "0.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot fastext-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/fastext_clojure/core.clj
+++ src/fastext_clojure/core.clj
@@ -1,9 +1,7 @@
 (ns fastext-clojure.core
-  (:require [clojure.core.matrix :refer :all]
-            [clojure.core.matrix.operators :refer :all])
+  (:use [uncomplicate.neanderthal core native]
+        [clojure.math.numeric-tower])
   (:gen-class))
-
-(set-current-implementation :vectorz)
 
 (defn make-ngram-hash
   [sequence-of-chars]
@@ -34,20 +32,45 @@
    {}
    words))
 
+(defn ->unigram-table
+  [counts]
+  (let [weighted-counts     (into {} (map-indexed (fn [idx count] [idx (expt count 0.5)]) counts))
+        sum-weighted-counts (apply + (vals weighted-counts))
+        table-size          10000000
+        unigram-table       (reduce
+                             (fn [unigram-table [index count]]
+                               (into unigram-table (repeat (/ (* count table-size) sum-weighted-counts) index)))
+                             []
+                             weighted-counts)]
+    (shuffle unigram-table)))
+
 (defn -main
   [& args]
 
   (with-open [rdr (clojure.java.io/reader (first args))]
     (let [file (line-seq rdr)]
-      (let [string-file (clojure.string/trim (first file))
-            words       (-> (clojure.string/split string-file #" " 1000001)
-                            (drop-last))
-            vocabulary  (->> (vals (->vocabulary words))
-                             (filter #(> (:count %) 5))
-                             (sort-by :count >))
-            hash->id    (into {} (map-indexed (fn [idx word] [(:word-hash word) idx]) vocabulary))
-            parsed-text (map #(get hash->id (hash [%]) nil) words)]
+      (let [string-file          (clojure.string/trim (first file))
+            words                (-> (clojure.string/split string-file #" " 1000001)
+                                     (drop-last))
+            vocabulary           (->> (vals (->vocabulary words))
+                                      (filter #(> (:count %) 5))
+                                      (sort-by :count >))
+            hash->id             (into {} (map-indexed (fn [idx word] [(:word-hash word) idx]) vocabulary))
+            parsed-text          (remove nil? (map #(get hash->id (hash [%]) nil) words))
+            nwords               (count vocabulary)
+            ntokens              (count words)
+            input-matrix         (dge (+ nwords 2000000) 100 (repeatedly #(/ (- (rand) 0.5) 50)))
+            output-matrix        (dge nwords 100)
+            unigram-table        (->unigram-table (map :count vocabulary))
+            discard-table        (map (fn [word]
+                                        (let [frequency          (/ (:count word) ntokens)
+                                              weighted-frequency (/ 0.0001 frequency)]
+                                          (+ (sqrt weighted-frequency) weighted-frequency))) vocabulary)
+            filtered-parsed-text (filter (fn [token] (> (rand) (nth discard-table token))) parsed-text)]
+
 
         ;;; TODO: REMEMBER TO TEST FOR NIL WHEN USING PARSED TEXT
 
-        (println (nth vocabulary 173))))))
+
+        (println (count parsed-text))
+        (println (count filtered-parsed-text))))))
